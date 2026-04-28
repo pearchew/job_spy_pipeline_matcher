af182fffe6b4c063b91178047b05099e026eb9c9
Pierson <chutlpierson@gmail.com>
2026-04-28 13:30:50 +0800

    Update run_job_match_pipeline.bat

--- run_job_match_pipeline.bat
+++ run_job_match_pipeline.bat
@@ -10,13 +10,10 @@ call venv\Scripts\activate.bat
 :: Run the main pipeline script
 python main.py
 
-echo Copying data to frontend...
-copy /Y output\matched_master_*.csv front_end\src\data\
-
-:: Push the newly generated CSVs to GitHub
+:: Push the newly generated CSVs (and frontend data) to GitHub
 echo.
 echo Pushing updates to GitHub...
-git add output/
+git add output/ front_end/src/data/
 git commit -m "Auto-update today's scraped jobs"
 git push
 
